0b324a0ae19d1cd31c26f1d07d4358e9bee1374b
pawelbubacz <pawel.bubacz@ext.auctane.com>
2025-04-23 14:12:28 +0200

    started tracking mikro-orm.config.ts

---
+++ infrastructure/config/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+
+export default defineConfig({
+  driver: PostgreSqlDriver,
+  dbName: 'users',
+  user: 'postgres',
+  password: 'bubuplug',
+  host: 'localhost',
+  port: 5432,
+  entities: ['./dist/src/entities/*.js'],
+  entitiesTs: ['./src/entities/*.ts'],
+  metadataProvider: TsMorphMetadataProvider,
+  debug: true,
+  allowGlobalContext: true
+});
